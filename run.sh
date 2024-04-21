770fba3a66cef995cfc968d4b0b1266316949366
Xu Boran <boran.xu@epfl.ch>
2024-04-21 12:10:19 +0200

    run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./run.sh </path/to/input.json> </path/to/simple_output.json> </path/to/pip_output.json>"
+    exit 1
+fi
+
+# Assign the arguments to variables
+input_path=$1
+simple_output_path=$2
+pip_output_path=$3
+
+# Run the Python script with the arguments
+python3 scheduler.py $input_path $simple_output_path $pip_output_path
